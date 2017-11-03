013857e97ba9f1e74b400c1b969d10d675140105
Osama Hassanein <player1os@gmail.com>
2017-11-03 18:03:14 +0100

    Update the base application a try out the demo

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { ApplicationPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('application App', () => {
-  let page: ApplicationPage;
+describe('my-app App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new ApplicationPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
